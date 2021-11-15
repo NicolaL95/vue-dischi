e00049bf7728294ea04776fd3829f8e89a449cf2
NicolaL95 <nicola.lilla@gmail.com>
2021-11-15 13:47:19 +0100

    Add Header components(css done)

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="header_container">
+    <img src="../assets/spotify-logo.svg" alt="" />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+#header_container {
+  background-color: #2e3a46;
+  padding: 10px 20px;
+  img {
+    width: 50px;
+  }
+}
+</style>
