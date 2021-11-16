4191333de43758ccd0c3b08a780c294baf8624e7
NicolaL95 <nicola.lilla@gmail.com>
2021-11-16 16:15:41 +0100

    Add $emit component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="main_container">
+    <Select @find="findGenre" />
     <SpotifyList></SpotifyList>
   </div>
 </template>
 
 <script>
 import SpotifyList from "./SpotifyList";
+import Select from "./Select";
 
 export default {
   components: {
     SpotifyList,
+    Select,
+  },
+  methods: {
+    findGenre() {
+      console.log("9");
+    },
   },
 };
 </script>
---
+++ src/components/Select.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="selector">
+    <select name="fil_gen" id="fil_gen" @change="$emit('find')">
+      <option value="volvo">1</option>
+      <option value="saab">2</option>
+      <option value="opel">3</option>
+      <option value="audi">4</option>
+    </select>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
